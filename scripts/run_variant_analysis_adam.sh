79ade962369871233de3a267177d541319a9aaf4
raopr <raopr@umkc.edu>
2020-07-19 19:25:59 -0500

    Scripts to enable variant analysis using Adam, Cannoli, Spark, Hadoop

---
+++ scripts/run_variant_analysis_adam.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: run_variant_analysis_adam.sh <HDFS-PATH-FASTQ_file1> <HDFS-PATH-FASTQ_file2> <cluster size>"
+fi
+
+SPARK_MASTER="spark://vm0:7077"
+CANNOLI_SUBMIT="./cannoli/bin/cannoli-submit"
+ADAM_SUBMIT="./adam/bin/adam-submit"
+HDFS_PREFIX="hdfs://vm0:9000"
+EXECUTOR_MEMORY=50g
+DRIVER_MEMORY=50g
+INPUT_FILE=mysequence
+REFERENCE="/mydata/hs38.fa"
+DICT="/mydata/hs38.dict"
+FREE_BAYES=${HOME}"/freebayes/bin/freebayes"
+
+let NUM_EXECUTORS=${3}
+let NUM_CORES=$(nproc)-4
+
+echo "👉 Interleaving FASTQ files."
+${CANNOLI_SUBMIT} --master ${SPARK_MASTER} --driver-memory ${DRIVER_MEMORY} --num-executors ${NUM_EXECUTORS} --executor-cores ${NUM_CORES} --executor-memory ${EXECUTOR_MEMORY} \
+    -- interleaveFastq ${1} ${2} ${HDFS_PREFIX}/${INPUT_FILE}.ifq
+
+echo "👉 Executing bwa for alignment."
+${CANNOLI_SUBMIT} --master ${SPARK_MASTER} --driver-memory ${DRIVER_MEMORY} --num-executors ${NUM_EXECUTORS} --executor-cores ${NUM_CORES} --executor-memory ${EXECUTOR_MEMORY} \
+    -- bwa ${HDFS_PREFIX}/${INPUT_FILE}.ifq ${HDFS_PREFIX}/${INPUT_FILE}.bam \
+    -executable ${HOME}/bwa/bwa -sample_id mysample -index ${REFERENCE} -sequence_dictionary ${DICT} -single
+
+echo "👉 Sorting and marking duplicates before variant calling."
+${ADAM_SUBMIT} --master ${SPARK_MASTER} --driver-memory ${DRIVER_MEMORY} --num-executors ${NUM_EXECUTORS} --executor-cores ${NUM_CORES} --executor-memory ${EXECUTOR_MEMORY} \
+    -- transformAlignments ${HDFS_PREFIX}/${INPUT_FILE}.bam ${HDFS_PREFIX}/${INPUT_FILE}.bam.adam \
+    -mark_duplicate_reads -sort_by_reference_position_and_index -reference ${REFERENCE}
+
+echo "👉 Variant calling using freebayes."
+${CANNOLI_SUBMIT} --master ${SPARK_MASTER} --driver-memory ${DRIVER_MEMORY} --num-executors ${NUM_EXECUTORS} --executor-cores ${NUM_CORES} --executor-memory ${EXECUTOR_MEMORY} \
+    -- freebayes ${HDFS_PREFIX}/${INPUT_FILE}.bam.adam ${HDFS_PREFIX}/${INPUT_FILE}.variants.adam \
+    -executable ${FREE_BAYES} -reference ${REFERENCE}
+
+echo "👉 Done with variant analysis. See ${HDFS_PREFIX}.variants.adam."
---
+++ scripts/setup_tools_cluster.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: copy_files <cluster size>"
+  exit
+fi
+
+# First copy to vm0
+cp -R  /proj/sparksecurity-PG0/hs38/* /mydata/
+cd ${HOME}
+git clone -b development https://github.com/MU-Data-Science/EVA.git
+${HOME}/EVA/scripts/setup_tools.sh
+
+# Then to the rest of the nodes
+for ((i=1;i<${1};i++));
+do
+  ssh vm${i} 'cd ${HOME}; git clone -b development https://github.com/MU-Data-Science/EVA.git; ${HOME}/EVA/scripts/setup_tools.sh'
+  scp /mydata/hs38.* vm${i}:/mydata/
+  echo ${i}
+done
