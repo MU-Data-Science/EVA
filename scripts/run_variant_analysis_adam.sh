1eb4f312c6878497ec51a0700677c5f74bcb56ba
raopr <raopr@umkc.edu>
2020-07-19 20:14:47 -0500

    Deleting HDFS files before running analysis; added date

--- scripts/run_variant_analysis_adam.sh
+++ scripts/run_variant_analysis_adam.sh
@@ -2,6 +2,7 @@
 
 if [[ $# -ne 3 ]]; then
     echo "Usage: run_variant_analysis_adam.sh <HDFS-PATH-FASTQ_file1> <HDFS-PATH-FASTQ_file2> <cluster size>"
+    exit
 fi
 
 SPARK_MASTER="spark://vm0:7077"
@@ -18,6 +19,12 @@ FREE_BAYES=${HOME}"/freebayes/bin/freebayes"
 let NUM_EXECUTORS=${3}
 let NUM_CORES=$(nproc)-4
 
+# Cleanup
+echo "👉 Deleting files..."
+hdfs dfs -rm -r ${HDFS_PREFIX}/${INPUT_FILE}.ifq ${HDFS_PREFIX}/${INPUT_FILE}.bam \
+    ${HDFS_PREFIX}/${INPUT_FILE}.bam.adam ${HDFS_PREFIX}/${INPUT_FILE}.variants.adam
+
+date
 echo "👉 Interleaving FASTQ files."
 ${CANNOLI_SUBMIT} --master ${SPARK_MASTER} --driver-memory ${DRIVER_MEMORY} --num-executors ${NUM_EXECUTORS} --executor-cores ${NUM_CORES} --executor-memory ${EXECUTOR_MEMORY} \
     -- interleaveFastq ${1} ${2} ${HDFS_PREFIX}/${INPUT_FILE}.ifq
@@ -38,3 +45,4 @@ ${CANNOLI_SUBMIT} --master ${SPARK_MASTER} --driver-memory ${DRIVER_MEMORY} --nu
     -executable ${FREE_BAYES} -reference ${REFERENCE}
 
 echo "👉 Done with variant analysis. See ${HDFS_PREFIX}.variants.adam."
+date
