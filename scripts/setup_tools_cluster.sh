690c7a1cc4d6cda173ca5a2d9e0f9f053d6c0601
raopr <raopr@umkc.edu>
2020-07-25 15:55:29 -0500

    Deleting EVA before cloning

--- scripts/setup_tools_cluster.sh
+++ scripts/setup_tools_cluster.sh
@@ -8,13 +8,13 @@ fi
 # First copy to vm0
 cp -R  /proj/sparksecurity-PG0/hs38/* /mydata/
 cd ${HOME}
-git clone -b development https://github.com/MU-Data-Science/EVA.git
+#git clone -b development https://github.com/MU-Data-Science/EVA.git
 ${HOME}/EVA/scripts/setup_bwa_freebayes.sh
 
 # Then to the rest of the nodes
 for ((i=1;i<${1};i++));
 do
-  ssh vm${i} 'cd ${HOME}; git clone -b development https://github.com/MU-Data-Science/EVA.git; ${HOME}/EVA/scripts/setup_bwa_freebayes.sh'
+  ssh vm${i} 'cd ${HOME}; rm -rf EVA; git clone -b development https://github.com/MU-Data-Science/EVA.git; ${HOME}/EVA/scripts/setup_bwa_freebayes.sh'
   scp /mydata/hs38.* vm${i}:/mydata/
   echo ${i}
 done
