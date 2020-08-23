3521a858f0ee95a7b739cf195566a8fa6855e066
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-08-23 14:28:43 -0500

    Minor SPAdes setup changes.

--- cluster_config/genome-tools-setup.sh
+++ cluster_config/genome-tools-setup.sh
@@ -48,9 +48,8 @@ echo "👉 Done with GATK copy 😎"
 SPADES_VERSION=3.14.1
 if [ ! -f $SHARE_DIR/EVA_Tools/SPAdes-${SPADES_VERSION}-Linux.tar.gz ]; then
     wget http://cab.spbu.ru/files/release${SPADES_VERSION}/SPAdes-${SPADES_VERSION}-Linux.tar.gz -P $SHARE_DIR/EVA_Tools/
-    gunzip $SHARE_DIR/EVA_Tools/SPAdes-${SPADES_VERSION}-Linux.tar.gz
 fi
-tar -xvf $SHARE_DIR/EVA_Tools/SPAdes-${SPADES_VERSION}-Linux.tar -C $DATA_DIR
+tar -xzf $SHARE_DIR/EVA_Tools/SPAdes-${SPADES_VERSION}-Linux.tar.gz -C $DATA_DIR
 ln -sf $DATA_DIR/SPAdes-${SPADES_VERSION}-Linux $HOME/spades
 echo "👉 Done with SPAdes setup 😎"
 
