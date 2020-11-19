e9bb3b85d7e6aa111fd2211084f39d9fe1387624
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-11-19 14:50:43 -0600

    Updating permission and data directory.

--- cluster_config/conda-setup.sh
+++ cluster_config/conda-setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Configurations.
 CONDA_URL="https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh"
+DATA_DIR="$1"
 
 # Download & Install Conda.
 wget $CONDA_URL -O $DATA_DIR/Anaconda3.sh
