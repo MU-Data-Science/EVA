4511e6491bb9012812c19bb49faa09eaaf4583f7
khawar <ks9dw@umsystem.edu>
2023-07-20 15:50:42 -0500

    Update copy_1kgenome_files.sh

--- scripts/copy_1kgenome_files.sh
+++ scripts/copy_1kgenome_files.sh
@@ -7,6 +7,15 @@ FILES=(
 "1000G_phase1.snps.high_confidence.hg38.vcf.gz.tbi"
 "hapmap_3.3.hg38.vcf.gz"
 "hapmap_3.3.hg38.vcf.gz.tbi"
+"hs38.dict"
+"hs38.fa"
+"hs38.fa.ann"
+"hs38.fa.img"  
+"hs38.fa.sa"
+"hs38.fa.amb"
+"hs38.fa.bwt"
+"hs38.fa.fai"
+"hs38.fa.pac"
 )
 
 for file in "${FILES[@]}"; do
